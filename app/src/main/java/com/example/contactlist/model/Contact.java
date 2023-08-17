5978151745a800eee83d281aa715880db90de759
Aline Souza <alinesouza.hrizon@gmail.com>
2023-08-16 21:48:12 -0300

    model: Cria model do contato

---
+++ app/src/main/java/com/example/contactlist/model/Contact.java
@@ -0,0 +1,37 @@
+package com.example.contactlist.model;
+
+public class Contact {
+    private int id;
+    private String name;
+    private String number;
+
+    public Contact(int id, String name, String number) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
