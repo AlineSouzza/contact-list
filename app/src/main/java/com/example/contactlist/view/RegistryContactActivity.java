d071438930ac0f8e91a725249c5cd6de5df76e4c
Aline Souza <alinesouza.hrizon@gmail.com>
2023-08-18 20:28:20 -0300

    view: Envia resultados obtidos na criação do contato para a lista via intent

--- app/src/main/java/com/example/contactlist/view/RegistryContactActivity.java
+++ app/src/main/java/com/example/contactlist/view/RegistryContactActivity.java
@@ -3,12 +3,22 @@ package com.example.contactlist.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.contactlist.R;
+import com.example.contactlist.model.Contact;
+
+import java.util.ArrayList;
 
 public class RegistryContactActivity extends AppCompatActivity {
+
+    private ArrayList<Contact> contactList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,18 +28,30 @@ public class RegistryContactActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        EditText name = findViewById(R.id.name_contact);
-        EditText number = findViewById(R.id.number_contact);
+        EditText etName = findViewById(R.id.name_contact);
+        EditText etNumber = findViewById(R.id.number_contact);
+        Button saveButton = findViewById(R.id.save_contact);
 
         String nameContact = getIntent().getStringExtra("ContactName");
         String numberContact = getIntent().getStringExtra("ContactNumber");
 
         if (nameContact != null) {
-            name.setText(nameContact);
+            etName.setText(nameContact);
         }
 
         if (numberContact != null) {
-            number.setText(numberContact);
+            etNumber.setText(numberContact);
         }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("NameContact", etName.getText().toString());
+                returnIntent.putExtra("NumberContact", etNumber.getText().toString());
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+            }
+        });
     }
 }
