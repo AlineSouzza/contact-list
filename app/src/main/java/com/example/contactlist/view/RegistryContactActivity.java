c4620a92a68a13dca36c70710c9dc4122d48e770
Aline Souza <as.alinedossantos.as@gmail.com>
2023-09-13 20:32:26 -0300

    feature: Adiciona validações para campo vazio e permissão de salvamento no banco de dados

--- app/src/main/java/com/example/contactlist/view/RegistryContactActivity.java
+++ app/src/main/java/com/example/contactlist/view/RegistryContactActivity.java
@@ -1,9 +1,12 @@
 package com.example.contactlist.view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +15,15 @@ import android.widget.EditText;
 
 import com.example.contactlist.R;
 import com.example.contactlist.model.Contact;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistryContactActivity extends AppCompatActivity {
-    private boolean isEditMode;
     private Contact contact;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,16 +45,61 @@ public class RegistryContactActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent returnIntent = new Intent();
                 String contactName = etName.getText().toString();
                 String contactNumber = etNumber.getText().toString();
 
-                contact.setName(contactName);
-                contact.setNumber(contactNumber);
+                if (contactName.equals("") || contactNumber.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                    builder.setMessage(R.string.info_ivalid_contact)
+                            .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.dismiss();
+                                }
+                            });
 
-                returnIntent.putExtra("Contact", contact);
-                setResult(Activity.RESULT_OK, returnIntent);
-                finish();
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                    return;
+
+                }
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                Map<String, Object> contact1 = new HashMap<String, Object>();
+                contact1.put("name", contactName);
+                contact1.put("number", contactNumber);
+
+                db.collection("Contacts").document().set(contact1)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                contact.setName(contactName);
+                                contact.setNumber(contactNumber);
+
+                                returnIntent.putExtra("Contact", contact);
+                                setResult(Activity.RESULT_OK, returnIntent);
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                                builder.setMessage(R.string.db_ivalid_contact)
+                                        .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                dialog.dismiss();
+                                            }
+                                        });
+
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
             }
         });
     }
